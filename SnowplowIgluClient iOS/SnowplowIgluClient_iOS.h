ed0b97af6e481afe5971d251eb5dfc09cd4a8c97
mhadam <michael@hadam.us>
2019-01-16 18:45:13 -0500

    Add support for Carthage

---
+++ SnowplowIgluClient iOS/SnowplowIgluClient_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SnowplowIgluClient_iOS.h
+//  SnowplowIgluClient iOS
+//
+//  Created by Michael Hadam on 1/16/19.
+//  Copyright © 2019 Snowplow Analytics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SnowplowIgluClient_iOS.
+FOUNDATION_EXPORT double SnowplowIgluClient_iOSVersionNumber;
+
+//! Project version string for SnowplowIgluClient_iOS.
+FOUNDATION_EXPORT const unsigned char SnowplowIgluClient_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnowplowIgluClient_iOS/PublicHeader.h>
+
+
---
+++ SnowplowIgluClient iOSTests/SnowplowIgluClient_iOSTests.m
@@ -0,0 +1,37 @@
+//
+//  SnowplowIgluClient_iOSTests.m
+//  SnowplowIgluClient iOSTests
+//
+//  Created by Michael Hadam on 1/16/19.
+//  Copyright © 2019 Snowplow Analytics. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SnowplowIgluClient_iOSTests : XCTestCase
+
+@end
+
+@implementation SnowplowIgluClient_iOSTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
